1ac88b55d28303ec6f014e1ae94b3470269c7b31
Jean-Samuel Reynaud <js@qowisio.com>
2018-10-24 12:38:30 +0200

    Allow recompute history

--- stat.sh
+++ stat.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
 PWD=$(dirname $0)
-DATETODOWNLOAD=$(date -d "2 days ago" +"%Y-%m-%d")
-MONTHTODOWNLOAD=$(date -d "2 days ago" +"%Y-%m")
-
+BACKDAY=6
+echo "Start"
+date
+TMP=$(mktemp)
+echo "Extrat data from $(date -d "$BACKDAY days ago" +"%Y-%m-%d") to now"
 if [ -z "$1" ]; then
-    TMP=$(mktemp)
-    echo "Extrat data for $DATETODOWNLOAD"
-    $PWD/stat.py $DATETODOWNLOAD > $TMP
-    mkdir -p $PWD/csv/${MONTHTODOWNLOAD}
-    rm -f $PWD/csv/${MONTHTODOWNLOAD}/${DATETODOWNLOAD}.txt $PWD/csv/${MONTHTODOWNLOAD}.txt
-    (head -1 ${TMP};grep "\"${DATETODOWNLOAD}\"" $TMP) > $PWD/csv/${MONTHTODOWNLOAD}/${DATETODOWNLOAD}.txt
-    rm -f ${TMP}
+    $PWD/stat.py $(date -d "$BACKDAY days ago" +"%Y-%m-%d") > $TMP
 fi
 
-##rm  $PWD/csv/OTHER.txt
-##(head -1 $TMP;grep -v "\"${DATETODOWNLOAD}\"" $TMP) > $PWD/csv/OTHER.txt
+for((hday=${BACKDAY};hday>=0;hday--)); do
+    DATETODOWNLOAD=$(date -d "$hday days ago" +"%Y-%m-%d")
+    MONTHTODOWNLOAD=$(date -d "$hday days ago" +"%Y-%m")
+    echo "Rebuild for ${DATETODOWNLOAD}"
 
-# Rebuild monthly stats
-(head -q -n 1 $PWD/csv/${MONTHTODOWNLOAD}/*.txt | head -1;tail -q -n +2 $PWD/csv/${MONTHTODOWNLOAD}/*.txt) > $PWD/csv/${MONTHTODOWNLOAD}.txt
-(
-echo '"Package","Item","Title","csv"'
-ls -1 $PWD/csv/*.txt | while read month; do
-    MONTH=$(basename $month .txt)
-    echo "\"Monthly\",\"$MONTH\",\"$(date -d "$MONTH-01" +"%B %Y")\",\"csv/${MONTH}.txt\""
+    mkdir -p $PWD/csv/${MONTHTODOWNLOAD}
+    rm -f $PWD/csv/${MONTHTODOWNLOAD}/${DATETODOWNLOAD}.txt $PWD/csv/${MONTHTODOWNLOAD}.txt
+    (head -1 ${TMP};grep "\"${DATETODOWNLOAD}\"" $TMP | sort) > $PWD/csv/${MONTHTODOWNLOAD}/${DATETODOWNLOAD}.txt
+
+    ##rm  $PWD/csv/OTHER.txt
+    ##(head -1 $TMP;grep -v "\"${DATETODOWNLOAD}\"" $TMP) > $PWD/csv/OTHER.txt
+
+    # Rebuild monthly stats
+    (head -q -n 1 $PWD/csv/${MONTHTODOWNLOAD}/*.txt | head -1;tail -q -n +2 $PWD/csv/${MONTHTODOWNLOAD}/*.txt | sort) > $PWD/csv/${MONTHTODOWNLOAD}.txt
+    (
+        echo '"Package","Item","Title","csv"'
+        ls -1 $PWD/csv/*.txt | while read month; do
+            MONTH=$(basename $month .txt)
+            echo "\"Monthly\",\"$MONTH\",\"$(date -d "$MONTH-01" +"%B %Y")\",\"csv/${MONTH}.txt\""
+        done
+
+        ls -1 $PWD/csv/*.txt | while read month; do
+            MONTH=$(basename $month .txt)
+            ls -1 $PWD/csv/$MONTH/*.txt | while read day; do
+                DAY=$(basename $day .txt)
+                echo "\"Daily for $(date -d "$MONTH-01" +"%B %Y")\",\"$DAY\",\"$(date -d "$DAY" +"%d %B %Y")\",\"csv/${MONTH}/${DAY}.txt\""
+            done
+        done
+    ) > $PWD/datasets.csv
+
 done
 
-ls -1 $PWD/csv/*.txt | while read month; do
-    MONTH=$(basename $month .txt)
-    ls -1 $PWD/csv/$MONTH/*.txt | while read day; do
-        DAY=$(basename $day .txt)
-        echo "\"Daily for $(date -d "$MONTH-01" +"%B %Y")\",\"$DAY\",\"$(date -d "$DAY" +"%d %B %Y")\",\"csv/${MONTH}/${DAY}.txt\""
-    done
-done
-) > $PWD/datasets.csv
+rm -f ${TMP}
+date
+echo "End"
